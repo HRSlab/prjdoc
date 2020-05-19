2d0d9df7a8039ed2c27b04dbc5fb95cb9c2c053d
HRS <44086439+HRSlab@users.noreply.github.com>
2020-05-19 19:46:09 +0200

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    themeConfig: {
+      logo: '/hero.png',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/features/' },
+        { text: 'External', link: 'https://google.com' }
+      ]
+    }
+  };
