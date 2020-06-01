6b87bb2b6e7bd1a713b5bb52276d28cb4cdc7cac
HRS <44086439+HRSlab@users.noreply.github.com>
2020-06-01 17:04:53 +0200

    redirectUri

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ module.exports = {
     ['vuepress-plugin-glossary'],
     [Auth, {
       domain: 'dev-zl1ksw63.eu.auth0.com',                           // Substitute your actual Auth0 domain.  Custom domains should work as well
-      redirectUri: 'http://localhost:8080/callback.html',    // Substitute the callback URL domain in your specific Application Config in the Auth0 portal. Make sure this url ends in `callback.html`
+      redirectUri: 'https://docshrs.netlify.app/callback.html',    // Substitute the callback URL domain in your specific Application Config in the Auth0 portal. Make sure this url ends in `callback.html`
       clientID: 'CXRqgoucCUhzQo7X6Ps4MkvzjTe5Kp4q'            // Substitute your actual Client Id
     }
     ], /// Comment Auth in Dev Mode
