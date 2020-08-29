99d259d7c6d8455ad126955073359f7166ebb049
HRS <44086439+HRSlab@users.noreply.github.com>
2020-08-29 12:57:22 +0200

    HubSpot Config

--- docs/.vuepress/components/Testprimer.vue
+++ docs/.vuepress/components/Testprimer.vue
@@ -3,15 +3,26 @@
 <div>
 
 <!-- Start of Meetings Embed Script -->
-<div class="meetings-iframe-container" data-src="https://app.hubspot.com/meetings/carlo7?embed=true"></div>
+<div class="meetings-iframe-container"  data-src="https://app.hubspot.com/meetings/carlo7?embed=true"></div>
 <script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js">
 </script>
+
 <!-- End of Meetings Embed Script -->
 
+<!-- Start of HubSpot Embed Code -->
+  <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/704325.js"></script>
+<!-- End of HubSpot Embed Code -->
 
 </div>
 
+
+
 </template>
+
+
+
+
+
 
 
 
