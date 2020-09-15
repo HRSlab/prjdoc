579ba7edabf240030833635de5cb1c4f5975824d
HRS <44086439+HRSlab@users.noreply.github.com>
2020-09-15 08:03:04 +0200

    links fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const Auth = require('vuepress-auth0'); // Comment This in Dev Mode
+ const Auth = require('vuepress-auth0'); // Comment This in Dev Mode
 
 
 module.exports = {
@@ -13,12 +13,12 @@ module.exports = {
     ['vuepress-plugin-reading-progress'],
     ['@vuepress/back-to-top'],
     ['vuepress-plugin-glossary'],
- [Auth, {
-     domain: 'dev-zl1ksw63.eu.auth0.com',                           // Substitute your actual Auth0 domain.  Custom domains should work as well
-  redirectUri: 'https://docshrs.netlify.app/callback.html',    // Substitute the callback URL domain in your specific Application Config in the Auth0 portal. Make sure this url ends in `callback.html`
-    clientID: 'emmXT3vGp47A7sraT3mogcGulzxpbBpH'            // Substitute your actual Client Id
-  }
-   ], /// Comment Auth in Dev Mode */
+   [Auth, {
+         domain: 'dev-zl1ksw63.eu.auth0.com',                           // Substitute your actual Auth0 domain.  Custom domains should work as well
+      redirectUri: 'https://docshrs.netlify.app/callback.html',    // Substitute the callback URL domain in your specific Application Config in the Auth0 portal. Make sure this url ends in `callback.html`
+        clientID: 'emmXT3vGp47A7sraT3mogcGulzxpbBpH'            // Substitute your actual Client Id
+      }
+       ], /// Comment Auth in Dev Mode */
     [
       'vuepress-plugin-copyright',
       {
@@ -27,18 +27,18 @@ module.exports = {
         authorName: "© 2012 - 2020 HRS Consulting Group."
       },
     ],
-    [ 'vuepress-plugin-git-log', 
-    {
-      additionalProps: {
-        subject: '%s',
-        authorEmail: '%ae',
-        authorRelative: '%ar',
-        dateShort: '%ad',
-        shortHash: '%t',
+    ['vuepress-plugin-git-log',
+      {
+        additionalProps: {
+          subject: '%s',
+          authorEmail: '%ae',
+          authorRelative: '%ar',
+          dateShort: '%ad',
+          shortHash: '%t',
+        },
       },
-    }, 
-  ],
-  [ 'flowchart' ],
+    ],
+    ['flowchart'],
   ],
 
   themeConfig: {
@@ -55,21 +55,22 @@ module.exports = {
         text: 'Progetto', link: '/projectsummary',
         meta: {                                             // The meta tag is required to let the plugin know you want to secure this nav route.
           auth: true
-        }                             
+        }
       },
-      { text: "proposta Commerciale", link: '/proposal/'},
+      { text: "proposta Commerciale", link: '/proposal/' },
       { text: 'Specifiche Tecniche', link: '/developers/' },
       { text: 'Contatti', link: '/contactus/' },
-    { text: 'Esci', link: 'https://dev-zl1ksw63.eu.auth0.com/v2/logout?returnTo=https://docshrs.netlify.app' },
+     { text: 'Esci', link: 'https://dev-zl1ksw63.eu.auth0.com/v2/logout?returnTo=https://docshrs.netlify.app' },
     ],
   },
   markdown: {
     extendMarkdown: md => {
       md.use(require('markdown-it-task-lists'));
     },
-    externalLinks: { 
-      target: '_self', 
-      rel: 'noopener noreferrer' 
-      },
+
+    externalLinks: {
+      target: '_self',
+      rel: ''
+    },
   },
 };
