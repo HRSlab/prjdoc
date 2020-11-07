6c21628330d8929a26c8a17633d83c608526a8da
Steve <44086439+SteveVercellin@users.noreply.github.com>
2020-11-07 11:49:31 +0100

    authentication enable

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-// const Auth = require('vuepress-auth0'); // Comment This in Dev Mode
+const Auth = require('vuepress-auth0'); // Comment This in Dev Mode
 
 module.exports = {
   title: "Project Documentation",
@@ -14,7 +14,7 @@ module.exports = {
     ['vuepress-plugin-reading-progress'],
     ['@vuepress/back-to-top'],
     ['vuepress-plugin-glossary'],
- /*   [Auth, {
+    [Auth, {
       domain: 'dev-zl1ksw63.eu.auth0.com',                           // Substitute your actual Auth0 domain.  Custom domains should work as well
       redirectUri: 'https://docshrs.netlify.app/callback.html',      // Substitute the callback URL domain in your specific Application Config in the Auth0 portal. Make sure this url ends in `callback.html`
       clientID: 'emmXT3vGp47A7sraT3mogcGulzxpbBpH'                   // Substitute your actual Client Id
@@ -52,16 +52,16 @@ module.exports = {
         text: ' ',
         link: '/',
       },
-/*       {
+      {
         text: 'Progetto', link: '/projectsummary',
          meta: {                                             // The meta tag is required to let the plugin know you want to secure this nav route.
           auth: true
         }
-      }, */
+      },
       { text: "proposta Commerciale", link: '/proposal/' },
       { text: 'Specifiche Tecniche', link: '/developers/' },
       { text: 'Contatti', link: '/contactus/' },
-      // { text: 'Esci', link: 'https://dev-zl1ksw63.eu.auth0.com/v2/logout?returnTo=https://docshrs.netlify.app', target: '_self', rel: false },
+      { text: 'Esci', link: 'https://dev-zl1ksw63.eu.auth0.com/v2/logout?returnTo=https://docshrs.netlify.app', target: '_self', rel: false },
     ],
   },
   markdown: {
