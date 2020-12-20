2e76c79dbf5ad425cd2aef337e3db770bb359594
Steve <44086439+SteveVercellin@users.noreply.github.com>
2020-12-20 14:26:36 +0100

    fixing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -61,7 +61,7 @@ module.exports = {
       { text: "proposta Commerciale", link: '/proposal/' },
       { text: 'Specifiche Tecniche', link: '/developers/' },
       { text: 'Contatti', link: '/contactus/' },
-      { text: 'Esci', link: 'https://dev-zl1ksw63.eu.auth0.com/v2/logout?returnTo=https://helpcutaway.hrslab.com', target: '_self', rel: false },
+      { text: 'Esci', link: 'https://dev-zl1ksw63.eu.auth0.com/v2/logout?returnTo=https://helpcutaway.hrslab.com/', target: '_self', rel: false },
     ],
   },
   markdown: {
