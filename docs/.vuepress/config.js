dffcf6bb24adfbe80a7b6a1fc139743a51cd7069
Steve <44086439+SteveVercellin@users.noreply.github.com>
2021-01-31 09:30:36 +0100

    fix git mess

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -59,14 +59,12 @@ module.exports = {
       },
       {
         text: 'Panoramica', link: '/projectsummary',
-        meta: {
-          auth: true
-        }     // The meta tag is required to let the plugin know you want to secure this nav route.
-      },
-      //      { text: "proposta Commerciale", link: '/proposal/' },
-      //      { text: 'Specifiche Tecniche', link: '/developers/' },
-      { text: "Dettagli", link: '/technicaldetails/' },
-      { text: "Programma di Manutenzione", link: '/maintenance/' },
+/*         //   meta: {
+        //     auth: true
+        //   }     // The meta tag is required to let the plugin know you want to secure this nav route.
+ */      },
+      { text: "proposta Commerciale", link: '/proposal/' },
+      { text: 'Specifiche Tecniche', link: '/developers/' },
       { text: "Analisi Tecnica", link: '/sonar/' },
       { text: 'Contatti', link: '/contactus/' },
       { text: 'Esci', link: 'https://dev-zl1ksw63.eu.auth0.com/v2/logout?returnTo=https://docs.energtech.hrslab.com', target: '_self', rel: false },
