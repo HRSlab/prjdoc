8b7314a56fc88044fac346eac0e02cc695fb7caa
Steve <44086439+SteveVercellin@users.noreply.github.com>
2021-07-10 10:22:49 +0200

    Plugin changes

--- docs/.vuepress/components/AxiosWp.vue
+++ docs/.vuepress/components/AxiosWp.vue
@@ -1,21 +1,60 @@
 <template>
-  <section class="my-3 py-4 px-3 olor-bg-secondary" data-color-mode="auto" data-light-theme="light" data-dark-theme="dark" >
-        <div class="Subhead px-3 color-bg-secondary">
+  <section
+    class="my-3 py-4 px-3 olor-bg-secondary"
+    data-color-mode="auto"
+    data-light-theme="light"
+    data-dark-theme="dark"
+  >
+    <div class="Subhead px-3 color-bg-secondary">
       <h3 class="Subhead-heading">Plugins Installati</h3>
     </div>
-  <div id="Box_Container">
-  <div data-color-mode="auto" data-light-theme="light" data-dark-theme="dark_dimmed"  class="Box Box--condensed text-small d-flex flex-column my-2 color-shadow-medium" v-for="plugin in plugins" :key="plugin">
-  <div class="Box-header">
-    <h4 class="Box-title">{{ plugin.name }}<span class="State State--small State--open float-right">{{ plugin.status }}</span></h4>
-  </div>
-  <div class="Box-body">
-    <p v-html="plugin.description.rendered"></p>
-  </div>
-  <div class="Box-footer">
-    <a :href="plugin.plugin_uri">{{ plugin.name }}</a><span class="Label float-right">Versione {{ plugin.version }}</span>
-  </div>
-  </div>
- </div>
+    <div id="Box_Container">
+      <div
+        data-color-mode="auto"
+        data-light-theme="light"
+        data-dark-theme="dark_dimmed"
+        class="
+          Box Box--condensed
+          text-small
+          d-flex
+          flex-column
+          my-2
+          color-shadow-medium
+        "
+        v-for="plugin in plugins"
+        :key="plugin"
+      >
+        <div class="Box-header">
+          <h4 class="Box-title"
+          v-if="plugin.name == 'ManageWP - Worker' "
+          > HRSlab WP Maintenance Plugin<span class="State State--small State--open float-right">{{
+          plugin.status
+          }}</span>
+          </h4>
+          <!-- V-ELSE -->
+          <h4 v-else class="Box-title">
+            {{ plugin.name
+            }}<span class="State State--small State--open float-right">{{
+              plugin.status
+            }}</span>
+          </h4>
+        </div>
+       <div class="Box-body">
+        <p v-if="plugin.name == 'ManageWP - Worker' ">
+          Questo è un plug-in avanzato che permette agl ingegneri
+          <a href="https://www.hrslab.com/" target="blank"> HRS </a> di eseguire tutte le attività di manutenzione senza
+          accedere al tuo sito. Per maggiori informazioni: <a href="mailto:developers@hrslab.com">developers@hrslab.com</a>
+        </p>
+          <p  v-else v-html="plugin.description.rendered"></p>
+        </div>
+        <div class="Box-footer">
+          <a  v-if="plugin.name == 'ManageWP - Worker'" href="https://www.hrslab.com/" target="blank">HRS lab</a>
+
+          <a v-else :href="plugin.plugin_uri">{{ plugin.name }}</a>
+          <span class="Label float-right">Versione {{ plugin.version }}</span>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -24,38 +63,35 @@ import axios from "axios";
 export default {
   data() {
     return {
-       plugins: []
+      plugins: [],
     };
   },
 
   beforeMount() {
-var config = {
-      method: 'get',
-      url: 'https://reinnovame.com/wp-json/wp/v2/plugins',
+    var config = {
+      method: "get",
+      url: "https://reinnovame.com/wp-json/wp/v2/plugins",
       headers: {
-        'Authorization': 'Basic SFJTRGV2OlZIOEJ6MFZZbWwzelFucnRDZVpLNTB5Uw=='
-      }
+        Authorization: "Basic SFJTRGV2OlZIOEJ6MFZZbWwzelFucnRDZVpLNTB5Uw==",
+      },
     };
-      axios(config)
-      .then(response => {
-       this.$data.plugins = response.data
-    })
+    axios(config)
+      .then((response) => {
+        this.$data.plugins = response.data;
+      })
       .then(function (response) {
         console.log(JSON.stringify(response.data));
       })
       .catch(function (error) {
         console.log(error);
       });
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-
 #Box_Container {
   max-height: 280px;
   overflow-y: scroll;
 }
-
-
 </style>
